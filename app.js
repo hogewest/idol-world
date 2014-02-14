4c25fe4bb0ca929590ea41f83846965138e05cb6
hogewest <hogewest@gmail.com>
2014-02-15 01:41:17 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+angular.module('app', [])
+
+.value('AppConfig', {
+  SPREADSHEET_KEY: "0AiYLrpOi8EdddGJtdTZyQm9MdUtvamxiNll2bUtTeGc",
+  TARGET_SHEET_NAME: "Sheet1",
+  BASE_URL: "http://spreadsheets.google.com/ccc?key="
+})
+
+.factory('Utils', function() {
+  return {
+    splitIntoRows: function(array, size) {
+      if (!Array.isArray(array) || size < 1) {
+        return array;
+      }
+
+      var rows= [];
+      var length = array.length;
+      for(var i = 0; i < length; i += size) {
+        rows.push(array.slice(i, i + size));
+      }
+      return rows;
+    },
+    mappingToIdol: function(rows) {
+      var idols = [];
+      $.each(rows, function(index, row){
+        idol = {
+          first_day: row.c[0].v == '○',
+          second_day: row.c[1].v == '○',
+          affiliation: row.c[2].v,
+          name: row.c[3].v,
+          character: row.c[4].v,
+          color: row.c[6].v,
+          style: row.c[6].p.style,
+          nickname: row.c[7].v,
+          song: row.c[8].v,
+          remark: row.c[9].v
+        };
+        idols.push(idol);
+      });
+      return idols;
+    }
+  };
+})
+
+.factory('query', function(AppConfig) {
+  var url = AppConfig.BASE_URL + AppConfig.SPREADSHEET_KEY + "&gid=0";
+  return new google.visualization.Query(url);
+})
+
+.controller('IdolCtrl', function($scope, Utils, query) {
+  query.send(function(response) {
+    var dataTable = JSON.parse(response.getDataTable().toJSON());
+    $scope.$apply(function() {
+      var idols = Utils.mappingToIdol(dataTable.rows.slice(1));
+      $scope.idolsRows = Utils.splitIntoRows(idols, 3);
+      console.log($scope.idolsRows);
+    });
+  });
+});
