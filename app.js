d6d6fe48e3d46f6aecafee8673459e0ef383b37b
hogewest <hogewest@gmail.com>
2014-02-18 23:51:49 +0900

    必要最低限のライブラリのみ使用するように変更

--- app.js
+++ app.js
@@ -19,30 +19,29 @@ angular.module('app', [])
       }
       return rows;
     },
-    mappingToIdol: function(rows) {
-      var idols = [];
-      $.each(rows, function(index, row){
-        idol = {
-          first_day: row.c[0].v == '○',
-          second_day: row.c[1].v == '○',
-          affiliation: row.c[2].v,
-          name: row.c[3].v,
-          link: row.c[16].v,
-          character: row.c[4].v,
-          character_link: row.c[18].v,
-          attribute: row.c[5].v,
-          color: row.c[6].v,
-          style: row.c[6].p.style.replace("font-family:Dialog;", ""),
-          nickname: row.c[7].v,
-          song: row.c[8].v,
-          call_song: row.c[9].v,
-          call_link: row.c[14].v,
-          song_link: row.c[11].v,
-          remark: row.c[12].v
-        };
-        idols.push(idol);
+    mappingToIdol: function(items) {
+      var result = [];
+      angular.forEach(items, function(item){
+        result.push({
+          first_day:      item.c[0].v == '○',
+          second_day:     item.c[1].v == '○',
+          affiliation:    item.c[2].v,
+          name:           item.c[3].v,
+          character:      item.c[4].v,
+          attribute:      item.c[5].v,
+          color:          item.c[6].v,
+          style:          item.c[6].p.style.replace("font-family:Dialog;", ""),
+          nickname:       item.c[7].v,
+          song:           item.c[8].v,
+          call_song:      item.c[9].v,
+          song_link:      item.c[11].v,
+          remark:         item.c[12].v,
+          call_link:      item.c[14].v,
+          link:           item.c[16].v,
+          character_link: item.c[18].v
+        });
       });
-      return idols;
+      return result;
     }
   };
 })
